c6adc46cd72621e3e2376d60b943011e7430481f
MIAACTON-STUDENT <G00428601@atu.ie>
2024-09-30 09:51:13 +0100

    Added new method called Add. I also pushed my Unversioned Files.

--- src/main/java/ie/atu/week1/Calculator.java
+++ src/main/java/ie/atu/week1/Calculator.java
@@ -1,4 +1,40 @@
 package ie.atu.week1;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        System.out.println();
+
+        Scanner scan1 = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+        double firstNum = scan1.nextDouble();
+        System.out.println("You entered: " + firstNum);
+
+        System.out.println("Please enter a second number: ");
+        double secondNum = scan1.nextDouble();
+        System.out.println("You entered: " + secondNum);
+
+        System.out.println("Please choose an operation (add, subtract, multiply, divide): ");
+        String operation = scan1.next();
+
+
+        boolean validOperation = true;
+        switch (operation) {
+            case "add":
+                add(firstNum, secondNum);
+                break;
+            default:
+                System.out.println("Invalid Operation.");
+                break;
+            }
+        }
+
+        public static void add(double first, double second) {
+        double result = first + second;
+            System.out.println("The total is " + result);
+    }
 }
+
+
